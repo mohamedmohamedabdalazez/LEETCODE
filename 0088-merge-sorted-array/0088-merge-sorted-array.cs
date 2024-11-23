79dd9543086be5022584accaa68af4d87783bdd1
Mohamed M . abd al azez <70726187+mohamedmohamedabdalazez@users.noreply.github.com>
2024-11-24 01:10:27 +0200

    Time: 0 ms (100%), Space: 46 MB (63.11%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cs
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -1,13 +1,6 @@
 public class Solution {
     public void Merge(int[] nums1, int m, int[] nums2, int n) {
-
-          
-            for (int i = 0; i < n; i++)
-                {
-                    nums1[m + i] = nums2[i];
-                }
-
-           
-            Array.Sort(nums1);
+        Array.Copy(nums2, 0, nums1, m, n);
+        Array.Sort(nums1);
     }
 }
