6f74b0e4e56d49f8c490a4714133845e9c6d93bf
Mohamed M . abd al azez <70726187+mohamedmohamedabdalazez@users.noreply.github.com>
2025-05-12 18:45:17 +0300

    Time: 0 ms (100%), Space: 40.4 MB (5.69%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cs
+++ 0058-length-of-last-word/0058-length-of-last-word.cs
@@ -1,6 +1,6 @@
 public class Solution {
-    public int LengthOfLastWord(string s) {
-        var clean = s.Trim();// this remove the first and the end spaces 
+    public int LengthOfLastWord(string ss) {
+        var clean = ss.Trim();// this remove the first and the end spaces 
         String [] str = clean.Split(" ");
         int ans = str[str.Length-1].Length;
         return ans;
